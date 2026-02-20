4257484c1ec7f3070da4c36778ebc220860c66dd
tehsheem <tehsheemayub@gmail.com>
2026-02-20 16:35:08 +0530

    Nesting age 80

---
+++ hello.py
@@ -0,0 +1,86 @@
+#a = True
+#b = True
+#print(a and b)
+
+#a = int("2")
+#print(type(a))
+
+#name = input("enter your name:")
+#print("Welcome", name)
+
+#val =  input("enter your name")
+#print(type(val), (val))
+
+#a = int(input("enter first number:"))
+#b = int(input("enter second number:"))
+#sum = a + b
+#print("sum :" , a+b)
+
+#a = int(input("enter f s"))
+
+#area = a * a
+#print("area:", area)
+
+#a = float(input("enter first number:"))
+#b = float(input("enter second number:"))
+#Average = a + b/2
+#print("Average:", Average)
+
+#a = int(input("enter first number:"))
+#b = int(input("enter second number:"))
+#print(a>=b)
+
+#a = "hello world\nhow are you"
+#print(a)
+
+#str = "hello"
+#str1 = "how are you"
+#con = str + str1
+#print(con)
+
+#str2 = "tehsheem"
+#print(str2)
+#print(len(str2))
+
+#str3 = "itachi"
+#print(str3[-6 : -1])
+
+#str = "im a student ew ew ew ew ew ew ew ew"
+#str = str.count("ew")
+#print(str)
+
+
+#a = input("enter your name")
+#name = a
+#print(a)
+#print(len(name))
+
+#a = "hello $ $ $ $ $ $ $"
+#print(a.count("$"))
+
+#light = "r"
+#if(light == "red"):
+  #  print("stop")
+#elif(light == "green"):
+ #   print("go")
+#elif(light == "yello"):
+  #  print("wait")
+#else:
+   # print("Warning :", "light is broken")
+   
+#age = 17
+#if(age >=18):
+   # print("you are eligible")
+#else:
+  #  print("eligibility:" , "you are not eligible")
+  
+marks = int(input("Enter Your Marks"))
+if(marks >= 90):
+    print("You got :" , "Grade A")
+elif(90 > marks >= 80):
+    print("You got:", "Grade B")
+elif(80 > marks >= 70):
+    print("You got:", "Grade C")
+else:
+    print("You got :", "Grade D")
+    
