749deac6d80a4336dae6182f58afa1058496d999
tehsheem <tehsheemayub@gmail.com>
2026-02-20 22:42:45 +0530

    list remove complete

---
+++ list.py
@@ -0,0 +1,17 @@
+# marks = [94.4, 45.2, 34.6, 98.4, 76.8]
+#print(type(marks))
+# print(marks)
+# print(marks[0])
+# print(len(marks))
+
+# student = ["Itachi", 78, 13, "Uchiha"]
+# print(student)
+# student[0] = "Kakashi"
+# print(student)
+
+# list = [1, 2, "amir", 4, 5, 6]
+# print(list[0:3])
+
+list = [3, 1, 2, 5, 6, 7, 8, 6]
+list.remove(6)
+print(list)
