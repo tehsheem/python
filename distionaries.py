5081aa14b9d32937e9abe0bd2aeab3fd6a41371f
tehsheem <tehsheemayub@gmail.com>
2026-02-22 17:31:22 +0530

    complete dict and sets

--- distionaries.py
+++ distionaries.py
@@ -12,13 +12,54 @@ info = {
 # info["salary"] = 1000
 # print(info)
 
-dict = {
-    "name" : "Itachi",
-    "subject" : {
-        "phy" : 90,
-        "chem" : 88,
-        "phython" : 100
-    }
-}
-dict.update({"specail" : "kamui"})
+# dict = {
+#     "name" : "Itachi",
+#     "subject" : {
+#         "phy" : 90,
+#         "chem" : 88,
+#         "phython" : 100
+#     }
+# }
+# dict.update({"specail" : "kamui"})
+# print(dict)
+
+# collection = {1, 2, 3, 4, 4}
+# print(type(collection))
+# print(collection)
+
+# collection = set()
+# print(type(collection))
+# collection.add(1)
+# collection.add(2)
+# collection.add(3)
+# collection.pop()
+# print(collection)
+
+# collection = {"hello", "world", "itachi", "kakashi"}
+# collection.pop()
+# print(collection)
+
+# set1 = {1, 2, 3}
+# set2 = {3, 4, 5}
+# print(set1.intersection(set2))
+ 
+# dict = {}
+# dict.add("table") = {"a piece of furniture", "list of facts and figures"}
+# dict.add("cat") =  "a small animal"
+# print(dict)
+
+dict = {}
+a = str(input("enter the sub"))
+b = int(input("enter thr marks"))
+c = str(input("enter the sub"))
+d = int(input("enter thr marks"))
+e = str(input("enter the sub"))
+f = int(input("enter thr marks"))
+ndict = {a : b}
+dict.update(ndict)
+g = {c : d}
+dict.update(g)
+u = {e: f}
+dict.update(u)
+
 print(dict)
