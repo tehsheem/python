676be4904026a0e71a8920406611988c2cba1b11
tehsheem <tehsheemayub@gmail.com>
2026-02-23 22:56:47 +0530

    complete loops range etc

--- loops.py
+++ loops.py
@@ -48,9 +48,75 @@
 #        print("finding...")
 #    i += 1
 
-i = 1
-while i <= 5:
-    print(i)
-    if(i == 3):
-        break
-    i += 1
+# i = 1
+# while i <= 5:
+#     print(i)
+#     if(i == 3):
+#         break
+#     i += 1
+
+# i = 0
+# while i <= 10:
+#     if(i%2 != 0):
+#        i += 1
+#        continue
+#     else:
+#        print(i)
+#        i += 1
+
+# nums = [1, 2, 3, 4, 5, 6,]
+
+# for val in nums:
+#     if(val == 2):
+#         print("2 found")
+#         break
+#     else:
+#         print(val)
+# print("end")
+
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# for num in nums:
+#     print(num)
+
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+# x = 16
+# ind = 0
+# for num in nums: 
+#     if(num == x):
+#         print(num , ind)
+#         break
+#     else:
+#         ind += 1
+
+# for b in range(100, 0, -1):
+#     print(b)
+
+# a = int(input("enter your number"))
+# for n in range(a , a*10+1, a):
+#     print(n)
+
+# for n in range(5):
+#     pass
+# print("end")
+
+# a = int(input("enter your n number"))
+# i = 0
+# for e in range(1, a+1):
+#         i += e
+# print(i)
+
+# a = int(input("enter the n value"))
+# sum = 1
+# i = 1
+# while i <= a:
+#     sum *= i
+#     i += 1
+# print(sum)
+
+a = int(input("enter the n value"))
+fact = 1
+for i in range(1, a+1):
+    fact *= i
+print(fact)
+    
